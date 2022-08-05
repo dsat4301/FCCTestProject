0f4114b95282de34c41f1e2f69c8971873ec70ae
David Stoll <david.stoll@optotech.net>
2022-08-05 08:17:09 +0200

    Add initial implementation

---
+++ FCCTestProject/NonGenericClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class NonGenericClass
+    {
+        public void Method()
+        {
+        }
+
+        public async Task MethodAsync()
+        {
+            await Task.Run(Method);
+        }
+    }
+}
---
+++ FCCTestProjectTests/GenericClass_T_Tests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using TestProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Tests
+{
+    public class GenericClass_T_Tests
+    {
+        [Fact()]
+        public void MethodTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
